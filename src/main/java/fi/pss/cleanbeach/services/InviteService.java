613701ee5bb117fdb0c302dc027ea3eab0ba60b0
thomas <thomas@vaadin.com>
2014-02-24 10:54:27 +0200

    fixes hibernate type issue

--- src/main/java/fi/pss/cleanbeach/services/InviteService.java
+++ src/main/java/fi/pss/cleanbeach/services/InviteService.java
@@ -19,56 +19,57 @@ import fi.pss.cleanbeach.data.UsersGroup;
 @Stateless
 public class InviteService {
 
-    @PersistenceContext(unitName = "cleanbeach")
-    private EntityManager entityManager;
+	@PersistenceContext(unitName = "cleanbeach")
+	private EntityManager entityManager;
 
-    /**
-     * Send an invite from <code>inviter</code> to <code>group</code> with
-     * <code>event</code> attached.
-     * 
-     * @param inviter
-     *            Inviter user
-     * @param group
-     *            Invitee group
-     * @param event
-     *            Event
-     * @return Invitation
-     */
-    public Invite invite(User inviter, UsersGroup group, Event event) {
-        Invite invite = new Invite();
-        invite.setInviter(inviter);
-        invite.setInvitee(group);
-        invite.setEvent(event);
-        entityManager.persist(invite);
-        return invite;
-    }
+	/**
+	 * Send an invite from <code>inviter</code> to <code>group</code> with
+	 * <code>event</code> attached.
+	 * 
+	 * @param inviter
+	 *            Inviter user
+	 * @param group
+	 *            Invitee group
+	 * @param event
+	 *            Event
+	 * @return Invitation
+	 */
+	public Invite invite(User inviter, UsersGroup group, Event event) {
+		Invite invite = new Invite();
+		invite.setInviter(inviter);
+		invite.setInvitee(group);
+		invite.setEvent(event);
+		entityManager.persist(invite);
+		return invite;
+	}
 
-    /**
-     * Returns all invitations for <code>group</code>.
-     * 
-     * @param group
-     *            Users group
-     * @return list of invitations
-     */
-    public Collection<Invite> getInvitations(UsersGroup group) {
-        TypedQuery<Invite> query = entityManager.createQuery(
-                "SELECT i from Invite i WHERE i.invitee=:group", Invite.class);
-        query.setParameter("group", group);
-        return query.getResultList();
-    }
+	/**
+	 * Returns all invitations for <code>group</code>.
+	 * 
+	 * @param group
+	 *            Users group
+	 * @return list of invitations
+	 */
+	public Collection<Invite> getInvitations(UsersGroup group) {
+		TypedQuery<Invite> query = entityManager.createQuery(
+				"SELECT i from Invite i WHERE i.invitee=:group", Invite.class);
+		query.setParameter("group", group);
+		return query.getResultList();
+	}
 
-    /**
-     * Returns pending invitations for <code>group</code>.
-     * 
-     * @param group
-     *            Users group
-     * @return list of invitations
-     */
-    public Collection<Invite> getPendingInvitations(UsersGroup group) {
-        TypedQuery<Invite> query = entityManager.createQuery(
-                "SELECT i from Invite i WHERE i.invitee=:group "
-                        + "AND i.accepted=0", Invite.class);
-        query.setParameter("group", group);
-        return query.getResultList();
-    }
+	/**
+	 * Returns pending invitations for <code>group</code>.
+	 * 
+	 * @param group
+	 *            Users group
+	 * @return list of invitations
+	 */
+	public Collection<Invite> getPendingInvitations(UsersGroup group) {
+		TypedQuery<Invite> query = entityManager.createQuery(
+				"SELECT i from Invite i WHERE i.invitee=:group "
+						+ "AND i.accepted=:accepted", Invite.class);
+		query.setParameter("group", group);
+		query.setParameter("accepted", false);
+		return query.getResultList();
+	}
 }
