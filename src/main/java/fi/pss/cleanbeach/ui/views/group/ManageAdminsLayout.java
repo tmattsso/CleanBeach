4e7979dcf6d5d3d5130d27c4b671ea7df10b0472
thomas <thomas@vaadin.com>
2014-04-08 22:13:14 +0200

    layout fix

--- src/main/java/fi/pss/cleanbeach/ui/views/group/ManageAdminsLayout.java
+++ src/main/java/fi/pss/cleanbeach/ui/views/group/ManageAdminsLayout.java
@@ -8,7 +8,8 @@ import com.vaadin.addon.touchkit.ui.NavigationView;
 import com.vaadin.addon.touchkit.ui.Switch;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
 
 import fi.pss.cleanbeach.data.User;
 import fi.pss.cleanbeach.data.UsersGroup;
@@ -22,20 +23,26 @@ public class ManageAdminsLayout extends NavigationView {
 
 		setCaption("Manage group admins");
 
-		VerticalLayout vl = new VerticalLayout();
+		GridLayout vl = new GridLayout(2, 1);
 		vl.setMargin(true);
 		vl.setSpacing(true);
+		vl.setColumnExpandRatio(0, 1);
+		vl.setWidth("100%");
 		setContent(vl);
 
 		List<User> users = new ArrayList<>(group.getMembers());
 		Collections.sort(users);
 		for (final User u : users) {
-			final Switch w = new Switch(u.getName());
+
+			Label l = new Label(u.getName());
+			vl.addComponent(l);
+
+			final Switch w = new Switch();
 			w.setValue(group.getAdmins().contains(u));
 			if (u.equals(group.getCreator())) {
 				w.setEnabled(false);
 				w.setValue(true);
-				w.setCaption(u.getName() + " (creator)");
+				l.setValue(u.getName() + " (creator)");
 			}
 			w.setWidth("100%");
 			w.addValueChangeListener(new ValueChangeListener() {
